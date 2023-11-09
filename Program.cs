c89b91eaee06f3709df724e2f27915f8c8741358
RavulaSonali <ravula.sonali@gmail.com>
2023-11-09 15:21:51 +0530

    Assignment 4-part3

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Assignment_4_part3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            //FIBONNACCI SERIES
+        {
+            Console.WriteLine("First 10 Numbers in Fibonacci Sequence:");
+            int n = 10;
+            int[] fibonacci = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                if (i <= 1)
+                {
+                    fibonacci[i] = i;
+                }
+                else
+                {
+                    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                }
+
+                Console.Write($"{fibonacci[i]} ");
+            }
+
+            Console.ReadLine();
+
+        }
+        }
+    }
+
+
